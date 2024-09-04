79191372bf4adea5b457be129e661d7ef83adac0
h3-Hanouzz <121695954+h3-Hanouzz@users.noreply.github.com>
2024-09-04 16:42:23 -0500

    Update scriptAMDRadeonPowerShell.bat

--- scriptAMDRadeonPowerShell.bat
+++ scriptAMDRadeonPowerShell.bat
@@ -5,7 +5,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 choco upgrade chocolatey
 
 :: Installs all other software
-choco install firefox steam temurin8 temurin17 temurin prismlauncher epicgameslauncher discord virtualbox gimp git blender davinciresolve paint.net obs-studio vlc curl
+choco install firefox steam temurin8 temurin17 temurin prismlauncher epicgameslauncher discord virtualbox gimp git blender davinciresolve paint.net obs-studio vlc curl winfetch
 
 :: Sets dark theme
 wt settings --set "defaultTheme" "Dark"
@@ -18,4 +18,5 @@ reg add "HKEY_CURRENT_USER\Control Panel\Keyboard" /v FilterKeys /t REG_DWORD /d
 :: Sets the wallpaper
 :: rundll32.exe user32.dll,SystemParametersInfo 0 0 <wallpaper_path> 0
 
+echo "Please install your graphics drivers
 
