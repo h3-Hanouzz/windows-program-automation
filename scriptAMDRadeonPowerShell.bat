4b85e8a03d7a5b97f65f9d3836ba74f9319c7adc
h3-Hanouzz <121695954+h3-Hanouzz@users.noreply.github.com>
2024-09-04 16:01:32 -0500

    Update scriptAMDRadeonPowerShell.bat

--- scriptAMDRadeonPowerShell.bat
+++ scriptAMDRadeonPowerShell.bat
@@ -1,5 +1,17 @@
 :: This is the script for people with AMD drivers using Windows 11. The only distintion here is which graphics drivers each file comes with.
 
+:: Activate Windows if not already activated, may require manual intervention
+setlocal enabledelayedexpansion
+
+:checkActivation
+for /f "tokens=2*" %%a in ('slmgr.vbs /xpr') do set "activationStatus=%%b"
+
+if "%activationStatus%"=="Permanent" (
+    echo Windows is activated.
+) else (
+    irm https://get.activated.win | iex
+) 
+
 :: Installs Chocolatey using PowerShell (Windows 11)
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco upgrade chocolatey
