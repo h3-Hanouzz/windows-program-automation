d51536e93b536de0a688472cceec64d7aa5eb978
h3-Hanouzz <121695954+h3-Hanouzz@users.noreply.github.com>
2024-09-04 16:18:17 -0500

    Update scriptAMDRadeonPowerShell.bat

--- scriptAMDRadeonPowerShell.bat
+++ scriptAMDRadeonPowerShell.bat
@@ -14,7 +14,7 @@ start AMD_Adrenaline.exe
 cd ..
 
 :: Installs all other software
-choco install firefox steam temurin8 temurin17 temurin prismlauncher epicgameslauncher discord virtualbox gimp git blender davinciresolve
+choco install firefox steam temurin8 temurin17 temurin prismlauncher epicgameslauncher discord virtualbox gimp git blender davinciresolve paint.net obs-studio vlc
 
 :: Sets dark theme
 wt settings --set "defaultTheme" "Dark"
@@ -25,6 +25,6 @@ reg add "HKEY_CURRENT_USER\Control Panel\Keyboard" /v ToggleKeys /t REG_DWORD /d
 reg add "HKEY_CURRENT_USER\Control Panel\Keyboard" /v FilterKeys /t REG_DWORD /d 0 /f
 
 :: Sets the wallpaper
-rundll32.exe user32.dll,SystemParametersInfo 0 0 <wallpaper_path> 0
+:: rundll32.exe user32.dll,SystemParametersInfo 0 0 <wallpaper_path> 0
 
 
