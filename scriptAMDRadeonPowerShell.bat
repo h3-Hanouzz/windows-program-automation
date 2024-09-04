6426fed2da9e1a0a555da8a0580b49eab1faede7
h3-Hanouzz <121695954+h3-Hanouzz@users.noreply.github.com>
2024-09-04 12:23:34 -0500

    Update scriptAMDRadeonPowerShell.bat

--- scriptAMDRadeonPowerShell.bat
+++ scriptAMDRadeonPowerShell.bat
@@ -3,3 +3,28 @@
 :: Installs Chocolatey using PowerShell (Windows 11)
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco upgrade chocolatey
+
+:: Installing curl is neccesary for this next part
+choco install curl
+
+:: Installs the AMD Adrenaline software
+cd Downloads
+curl -o -L AMD_Adrenaline.exe https://drivers.amd.com/drivers/installer/24.10/whql/amd-software-adrenalin-edition-24.8.1-minimalsetup-240829_web.exe
+start AMD_Adrenaline.exe
+cd ..
+
+:: Installs all other software
+choco install firefox steam temurin8 temurin17 temurin prismlauncher epicgameslauncher discord virtualbox gimp git blender davinciresolve
+
+:: Sets dark theme
+wt settings --set "defaultTheme" "Dark"
+
+:: Disables the "Allow the shortcut key to start (Sticky/Toggle/Filter) keys"? settings
+reg add "HKEY_CURRENT_USER\Control Panel\Keyboard" /v StickyKeys /t REG_DWORD /d 0 /f
+reg add "HKEY_CURRENT_USER\Control Panel\Keyboard" /v ToggleKeys /t REG_DWORD /d 0 /f
+reg add "HKEY_CURRENT_USER\Control Panel\Keyboard" /v FilterKeys /t REG_DWORD /d 0 /f
+
+:: Sets the wallpaper
+rundll32.exe user32.dll,SystemParametersInfo 0 0 <wallpaper_path> 0
+
+
